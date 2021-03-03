c3fe80aba69a42649a41a672053643701f5ed0da
Roberto D'Auria <roberto@measurementlab.net>
2021-03-03 04:18:21 +0100

    .

--- app/libraries/ndt7-upload-worker.js
+++ app/libraries/ndt7-upload-worker.js
@@ -80,7 +80,7 @@ const uploadTest = function(sock, postMessage, now) {
       return;
     }
 
-    const maxMessageSize = 1048576; /* = (1<<20) = 1MB */
+    const maxMessageSize = 65536; /* = (1<<16) = 1MB */
     const nextSizeIncrement =
         (data.length >= maxMessageSize) ? Infinity : 16 * data.length;
     if (total >= nextSizeIncrement) {
